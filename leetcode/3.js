e26803f5c02cf29ae17606609dd5d219d7aada18
shm_usuf <shmusuf@gmail.com>
2023-06-17 19:15:34 +0600

    3, 389 added

---
+++ leetcode/3.js
@@ -0,0 +1,25 @@
+/* Given a string s, find the length of the longest 
+substrin
+ without repeating characters.
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function lengthOfLongestSubstring(str) {
+  let longest = "";
+  for (let i = 0; i < str.length; i++) {
+    if (longest.includes(str[i])) {
+      return longest.length;
+    } else {
+      longest += str[i];
+    }
+  }
+  return longest.length;
+}
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+/* Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3. */
