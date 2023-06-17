b2bc9d97a79e1d429cebc36ee80db32b4c2c3c4a
shm_usuf <shmusuf@gmail.com>
2023-06-17 21:56:24 +0600

    13, 50 added

---
+++ leetcode/13.js
@@ -0,0 +1,22 @@
+/* Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M. */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+  const map = { 1: I, 5: V, 10: X, 50: L, 100: C, 500: D, 1000: M };
+
+  let result = 0;
+  s.split("").forEach((num, i) => {
+    if (map[num] < map[s[i - 1]]) result -= map[num];
+    else result += map[num];
+  });
+
+  return result;
+};
+
+console.log(romanToInt(3));
+/* Input: s = "III";
+Output: 3;
+Explanation: III = 3; */
---
+++ leetcode/50.js
@@ -0,0 +1,31 @@
+function myPow(x, n) {
+  if (n === 0) {
+    return 1;
+  }
+
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+
+  let result = 1;
+  while (n > 0) {
+    if (n % 2 === 1) {
+      result *= x;
+    }
+    x *= x;
+    n = Math.floor(n / 2);
+  }
+
+  return result;
+}
+
+console.log(myPow(2.0, -2));
+
+/* Input: x = 2.10000, n = 3
+Output: 9.26100
+Example 3:
+
+Input: x = 2.00000, n = -2
+Output: 0.25000
+Explanation: 2-2 = 1/22 = 1/4 = 0.25 */
