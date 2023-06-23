44750854caabf3dbc1c7015f0cac26471a8a61d3
shm_usuf <shmusuf@gmail.com>
2023-06-23 13:59:58 +0600

    leetcode 48 added

---
+++ leetcode/48.js
@@ -0,0 +1,29 @@
+/* You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation */
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+  for (let r = 0; r < matrix.length; r++) {
+    for (let c = r; c < matrix[0].length; c++) {
+      [matrix[r][c], matrix[c][r]] = [matrix[c][r], matrix[r][c]];
+    }
+  }
+
+  for (let row of matrix) {
+    row.reverse();
+  }
+};
+
+/* Input: matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+Output: [
+  [7, 4, 1],
+  [8, 5, 2],
+  [9, 6, 3],
+]; */
