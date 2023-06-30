3d27c73c4d931917ee1326c10ca501b02a1693e1
shm_usuf <shmusuf@gmail.com>
2023-06-30 22:32:03 +0600

    leetcode 374 added

---
+++ leetcode/374.js
@@ -0,0 +1,44 @@
+/* We are playing the Guess Game. The game is as follows:
+
+I pick a number from 1 to n. You have to guess which number I picked.
+
+Every time you guess wrong, I will tell you whether the number I picked is higher or lower than your guess.
+
+You call a pre-defined API int guess(int num), which returns three possible results:
+
+-1: Your guess is higher than the number I picked (i.e. num > pick).
+1: Your guess is lower than the number I picked (i.e. num < pick).
+0: your guess is equal to the number I picked (i.e. num == pick).
+Return the number that I picked.
+
+  */
+/**
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function (n) {
+  let left = 1;
+  let right = n;
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    let result = guess(mid);
+    if (result === 0) {
+      return mid;
+    }
+    if (result === -1) {
+      right = mid - 1;
+    }
+    if (result === 1) {
+      left = mid + 1;
+    }
+  }
+};
