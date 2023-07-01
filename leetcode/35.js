ace4cdf7686ab1396066f0dfb16a216fbb07e7a3
shm_usuf <shmusuf@gmail.com>
2023-07-01 22:30:58 +0600

    leetcode 35 added

---
+++ leetcode/35.js
@@ -0,0 +1,24 @@
+/* Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+You must write an algorithm with O(log n) runtime complexity. */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  if (target < nums[left]) return 0;
+  if (target > nums[right]) return nums.length;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] < target) {
+      left = mid + 1;
+    }
+    if (nums[mid] > target) {
+      right = mid - 1;
+    }
+  }
+  return left;
+};
