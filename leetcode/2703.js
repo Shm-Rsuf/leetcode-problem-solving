97a5c004f9a9453db83546743a99bb24fe0ea0b6
shm_usuf <shmusuf@gmail.com>
2023-07-12 14:18:57 +0600

    leetcode 2703 added

---
+++ leetcode/2703.js
@@ -0,0 +1,12 @@
+/* Write a function argumentsLength that returns the count of arguments passed to it. */
+
+/**
+ * @return {number}
+ */
+var argumentsLength = function (...args) {
+  return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
