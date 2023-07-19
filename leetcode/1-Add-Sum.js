165bc0016a0b657429f603c46a102d4c5a5410aa
shm_usuf <shmusuf@gmail.com>
2023-07-19 23:58:53 +0600

    leetcode 1 added

--- leetcode/1-Add-Sum.js
+++ leetcode/1-Add-Sum.js
@@ -1,3 +1,8 @@
+/* Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order. */
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -13,7 +18,3 @@ var twoSum = function (nums, target) {
   }
   return result;
 };
-
-console.log(twoSum([2, 5, 7], 7));
-console.log(twoSum([2, 5, 7], 9));
-console.log(twoSum([3, 2, 4], 6));
