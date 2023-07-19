4e7f4096599213fe53db8237808eafa8ccb9edac
shm_usuf <shmusuf@gmail.com>
2023-07-19 15:44:02 +0600

    leetcode 349 added

--- leetcode/349.js
+++ leetcode/349.js
@@ -1,13 +1,17 @@
 var intersect = function (nums1, nums2) {
+  let set1 = new Set(nums1);
+  let set2 = new Set(nums2);
   let res = [];
-  for (var i = 0; i < nums2.length; i++) {
-    if (nums1.includes(nums2[i])) {
-      res.push(nums2[i]);
-      nums1.splice(nums1.indexOf(nums2[i]), 1);
+
+  for (let value of set1) {
+    if (set2.has(value)) {
+      res.push(value);
     }
   }
   return res;
 };
+
+console.log(intersect([4, 9, 5], [9, 4, 9, 8, 4]));
 
 /* Example 1:
 Input: nums1 = [1,2,2,1], nums2 = [2,2]
