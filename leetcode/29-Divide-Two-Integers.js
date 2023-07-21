02ecfda72915779dbfdfc5904d1d5caa1a3d450a
shm_usuf <shmusuf@gmail.com>
2023-07-21 21:12:40 +0600

    leetcode 29 added

--- leetcode/29-Divide-Two-Integers.js
+++ leetcode/29-Divide-Two-Integers.js
@@ -1,3 +1,10 @@
+/* Given two integers dividend and divisor, divide two integers without using multiplication, division, and mod operator.
+
+The integer division should truncate toward zero, which means losing its fractional part. For example, 8.345 would be truncated to 8, and -2.7335 would be truncated to -2.
+
+Return the quotient after dividing dividend by divisor.
+
+Note: Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231, 231 − 1]. For this problem, if the quotient is strictly greater than 231 - 1, then return 231 - 1, and if the quotient is strictly less than -231, then return -231. */
 /**
  * @param {string} s
  * @return {boolean}
@@ -15,7 +22,11 @@ var isPalindrome = function (s) {
   return string1 === string2.reverse();
 };
 
-console.log(isPalindrome("mama"));
-console.log(isPalindrome("A man, a plan, a canal: Panama"));
-console.log(isPalindrome(""));
-console.log(isPalindrome("race a car"));
+/* 
+Input: dividend = 10, divisor = 3
+Output: 3
+Explanation: 10/3 = 3.33333.. which is truncated to 3.
+
+Input: dividend = 7, divisor = -3
+Output: -2
+Explanation: 7/-3 = -2.33333.. which is truncated to -2. */
