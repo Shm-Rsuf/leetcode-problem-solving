50d6482eb6fee4a4af34d68946d8bb7019c22409
shm_usuf <shmusuf@gmail.com>
2023-07-21 21:05:43 +0600

    leetcode 9 added

--- leetcode/9-Palindrome-Number.js
+++ leetcode/9-Palindrome-Number.js
@@ -1,3 +1,4 @@
+/* Given an integer x, return true if x is a palindrome and false otherwise */
 /**
  * @param {number} x
  * @return {boolean}
@@ -8,3 +9,13 @@ var isPalindrome = function (x) {
   let reversedStr = intgetStr.split("").reverse().join("");
   return intgetStr === reversedStr;
 };
+
+/* 
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+*/
