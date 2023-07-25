f40939c8f1e037c9d7d4487fd5d467dd10f95fe4
shm_usuf <shmusuf@gmail.com>
2023-07-25 20:52:05 +0600

    leetcode 2678 added

--- leetcode/2678.js
+++ leetcode/2678.js
@@ -19,7 +19,7 @@ var countSeniors = function (details) {
   }
   return count;
 };
-console.log(
-  countSeniors(["7868190130M7522", "5303914400F9211", "9273338290F4010"])
-);
-console.log(countSeniors(["1313579440F2036", "2921522980M5644"]));
+// console.log(
+//   countSeniors(["7868190130M7522", "5303914400F9211", "9273338290F4010"])
+// );
+// console.log(countSeniors(["1313579440F2036", "2921522980M5644"]));
