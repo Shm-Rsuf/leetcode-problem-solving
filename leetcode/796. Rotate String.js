6cbf4fd8dbd8b70be6e267830e9a267574599a19
shm_usuf <shmusuf@gmail.com>
2023-08-18 10:39:44 +0600

    leetcode 796 added

---
+++ leetcode/796. Rotate String.js
@@ -0,0 +1,28 @@
+/* 
+Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+A shift on s consists of moving the leftmost character of s to the rightmost position.
+For example, if s = "abcde", then it will be "bcdea" after one shift.
+ */
+
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function (s, goal) {
+  if (s.length !== goal.length) return false;
+  s = s + s;
+  return s.includes(goal);
+};
+
+console.log(rotateString("abcde", "cdeab"));
+console.log(rotateString("abcde", "abced"));
+
+/*
+Input: s = "abcde", goal = "cdeab"
+Output: true
+
+Input: s = "abcde", goal = "abced"
+Output: false 
+ */
