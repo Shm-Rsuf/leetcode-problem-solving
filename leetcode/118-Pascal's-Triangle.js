d90c8806a44f420cf42cc34782674e897ff1337a
shm_usuf <shmusuf@gmail.com>
2023-09-06 23:38:10 +0600

    leetcode 118 added

--- leetcode/118-Pascal's-Triangle.js
+++ leetcode/118-Pascal's-Triangle.js
@@ -19,9 +19,6 @@ const generate = function (numRows) {
   return arr;
 };
 
-console.log(generate(5));
-console.log(generate(1));
-
 /* Input: numRows = 5
 Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 Example 2:
