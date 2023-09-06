8bad13f8bad302210b98a426eb0fdded742b7f8f
shm_usuf <shmusuf@gmail.com>
2023-09-06 23:34:19 +0600

    leetcode 50 added

--- leetcode/50.js
+++ leetcode/50.js
@@ -22,10 +22,11 @@ function myPow(x, n) {
 
 console.log(myPow(2.0, -2));
 
-/* Input: x = 2.10000, n = 3
+/* 
+Input: x = 2.10000, n = 3
 Output: 9.26100
-Example 3:
 
 Input: x = 2.00000, n = -2
 Output: 0.25000
-Explanation: 2-2 = 1/22 = 1/4 = 0.25 */
+Explanation: 2-2 = 1/22 = 1/4 = 0.25
+*/
