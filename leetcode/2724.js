eaf4de3d41a8fddc1dac21540de19ddc4e85b9b3
shm_usuf <shmusuf@gmail.com>
2023-09-11 21:50:23 +0600

    leetcode 2724 re-added

--- leetcode/2724.js
+++ leetcode/2724.js
@@ -12,12 +12,11 @@ var sortBy = function (arr, fn) {
 };
 
 /*
-Example 1:
 Input: arr = [5, 4, 1, 2, 3], fn = (x) => x
 Output: [1, 2, 3, 4, 5]
 Explanation: fn simply returns the number passed to it so the array is sorted in ascending order.
 
-Example 2:
 Input: arr = [{"x": 1}, {"x": 0}, {"x": -1}], fn = (d) => d.x
 Output: [{"x": -1}, {"x": 0}, {"x": 1}]
-Explanation: fn returns the value for the "x" key. So the array is sorted based on that value. */
+Explanation: fn returns the value for the "x" key. So the array is sorted based on that value. 
+*/
