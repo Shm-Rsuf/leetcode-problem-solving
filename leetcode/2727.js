1f9c71b0e8da3be7cca736249494c4083ecae318
shm_usuf <shmusuf@gmail.com>
2023-09-11 21:49:28 +0600

    leetcode 2727 re-added

--- leetcode/2727.js
+++ leetcode/2727.js
@@ -17,18 +17,16 @@ var isEmpty = function (obj) {
   }
 };
 
-/* 
-Example 1:
+/*
 Input: obj = {"x": 5, "y": 42}
 Output: false
 Explanation: The object has 2 key-value pairs so it is not empty.
 
-Example 2:
 Input: obj = {}
 Output: true
 Explanation: The object doesn't have any key-value pairs so it is empty.
 
-Example 3:
 Input: obj = [null, false, 0]
 Output: false
-Explanation: The array has 3 elements so it is not empty. */
+Explanation: The array has 3 elements so it is not empty. 
+*/
