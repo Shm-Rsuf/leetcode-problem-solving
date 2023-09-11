ac60e1cf912933abb16aa60b67582f8c6acee0e5
shm_usuf <shmusuf@gmail.com>
2023-09-11 21:46:51 +0600

    leetcode 2810 re-added

--- leetcode/2810.js
+++ leetcode/2810.js
@@ -23,4 +23,15 @@ var finalString = function (s) {
   return result.join("");
 };
 
-console.log(finalString("string"));
+/* 
+Input: s = "string"
+Output: "rtsng"
+Explanation: 
+After typing first character, the text on the screen is "s".
+After the second character, the text is "st". 
+After the third character, the text is "str".
+Since the fourth character is an 'i', the text gets reversed and becomes "rts".
+After the fifth character, the text is "rtsn". 
+After the sixth character, the text is "rtsng". 
+Therefore, we return "rtsng". 
+*/
