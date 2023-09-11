57217566ff0d575b95fc12feb3831ec60d08c74b
shm_usuf <shmusuf@gmail.com>
2023-09-11 22:01:28 +0600

    leetcode 2703 re-added

--- leetcode/2703.js
+++ leetcode/2703.js
@@ -7,6 +7,11 @@ var argumentsLength = function (...args) {
   return args.length;
 };
 
-/**
- * argumentsLength(1, 2, 3); // 3
+/*
+ Input: argsArr = [5]
+Output: 1
+Explanation:
+argumentsLength(5); // 1
+
+One value was passed to the function so it should return 1.
  */
